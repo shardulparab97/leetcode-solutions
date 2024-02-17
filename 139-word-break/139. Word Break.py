720d1d8f3a7b37bab7b70a614f6182a9857f54d4
Shardul Parab <shardulSVparab@gmail.com>
2024-02-17 15:45:32 -0500

    Runtime: 35 ms (87.47%), Memory: 17.8 MB (6.29%) - Gitcode

--- 139-word-break/139. Word Break.py
+++ 139-word-break/139. Word Break.py
@@ -1,15 +1,41 @@
+class Node:
+    def __init__(self):
+        self.isWord = False
+        self.children = dict()
+
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        dp = [False] * len(s)
-        for i in range(len(s)):
-            for word in wordDict:
-                # Handle out of bounds case
-                if i < len(word) - 1:
-                    continue
-                
-                if i == len(word) - 1 or dp[i - len(word)]:
-                    if s[i - len(word) + 1:i + 1] == word:
-                        dp[i] = True
-                        break
+        wordDict = set(wordDict)
+        root = Node()
+
+        # 0(mk) time complexity 
+        # making the trie
+        for word in wordDict:
+            node = root
+            for w in word:
+                if w not in node.children:
+                    node.children[w] = Node()
+                node = node.children[w]
+            node.isWord = True
+
+        len_s = len(s)
+        dp = [False] * len_s
+
+        for i in range(len_s):
+            if i == 0 or dp[i-1] == True:
+                curr = root # means we have found a word here
+                for j in range(i, len_s):
+                        ch = s[j]
+                        if ch in curr.children:
+                            curr = curr.children[ch]
+                            if curr.isWord == True:
+                                dp[j] = True 
+                                if j == len_s - 1:
+                                    return True
+                        else:
+                            break
 
         return dp[-1]
+                    
+
+        
