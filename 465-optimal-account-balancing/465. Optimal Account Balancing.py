c2f749a3ea130ed53e0135b9fe827c78d23a1bac
Shardul Parab <shardulSVparab@gmail.com>
2024-02-20 13:49:21 -0500

    Runtime: 53 ms (88.09%), Memory: 16.6 MB (44.15%) - Gitcode

--- 465-optimal-account-balancing/465. Optimal Account Balancing.py
+++ 465-optimal-account-balancing/465. Optimal Account Balancing.py
@@ -1,63 +1,30 @@
 class Solution:
     def minTransfers(self, transactions: List[List[int]]) -> int:
-        # backtracking 
-        
-        balance = collections.defaultdict(int)
+        # https://leetcode.com/problems/optimal-account-balancing/solutions/219187/short-o-n-2-n-dp-solution-with-detailed-explanation-and-complexity-analysis/ 
+        balances = collections.defaultdict(int)
 
         for t in transactions:
-            balance[t[0]] -= t[2]
-            balance[t[1]] += t[2]
-        balance = [v for v in balance.values() if v!=0]
-        # print(balance)
-        n = len(balance)
-        def solve(idx):
-            # move to the next non zero option
-            while idx<n and balance[idx] == 0:
-                idx += 1
-            
-            if idx == n:
-                return 0
-
-            ans = float("inf")
-            for t in range(idx+1, n):
-                # if there needs to be a transactions
-                if balance[idx] * balance[t] < 0:
-                    balance[t] += balance[idx]
-                    ans = min(ans, 1+solve(idx+1))
-                    balance[t] -= balance[idx]
-            return ans
-        # print("here")
-        # ans = solve(0)
-        # print(ans)
-        # return 0
-        return solve(0)
-
-        # balance_map = collections.defaultdict(int)
-        # for a, b, amount in transactions:
-        #     balance_map[a] += amount
-        #     balance_map[b] -= amount
+            balances[t[0]] -= t[2]
+            balances[t[1]] += t[2]
         
-        # balance_list = [amount for amount in balance_map.values() if amount]
-        # n = len(balance_list)
-        # print(balance_list)
-        
-        # def dfs(cur):
-        #     while cur < n and not balance_list[cur]:
-        #         cur += 1
-        #     if cur == n:
-        #         return 0
-        #     cost = float('inf')
-        #     for nxt in range(cur + 1, n):
-        #         # If nxt is a valid recipient, do the following: 
-        #         # 1. add cur's balance to nxt.
-        #         # 2. recursively call dfs(cur + 1).
-        #         # 3. remove cur's balance from nxt.
-        #         if balance_list[nxt] * balance_list[cur] < 0:
-        #             balance_list[nxt] += balance_list[cur]
-        #             cost = min(cost, 1 + dfs(cur + 1))
-        #             balance_list[nxt] -= balance_list[cur]
-        #     return cost
-        # return dfs(0)
+        balances = [v for v in balances.values() if v!=0]
+
+        n = len(balances)
+        dp = [0] * (2**n) # dp[-1] will be when all are set/ all are available
+        sums = [0] * (2**n)
+
+        for mask in range(2**n): # looping over all the masks
+            set_bit = 1
+            for b in range(n): # setting 1 bit at a time
+                if set_bit & mask == 0: # means it was unset previously
+                    nxt = mask | set_bit
+                    sums[nxt] = sums[mask] + balances[b]
+                    if sums[nxt] == 0: # means we have found a useful subset
+                        dp[nxt] = max(dp[nxt], dp[mask] + 1) 
+                    else:
+                        dp[nxt] = max(dp[nxt], dp[mask])
+                set_bit = set_bit<<1
+        return n - dp[-1]
 
 
         
