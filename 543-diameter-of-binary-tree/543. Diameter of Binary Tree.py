23acee82735f6a6f13f8c82df799e4adb9c19ba9
Shardul Parab <shardulSVparab@gmail.com>
2024-02-26 20:57:16 -0500

    Runtime: 44 ms (51.24%), Memory: 17.7 MB (74.69%) - Gitcode

--- 543-diameter-of-binary-tree/543. Diameter of Binary Tree.py
+++ 543-diameter-of-binary-tree/543. Diameter of Binary Tree.py
@@ -5,20 +5,23 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def solve(self, root):
-        if root == None:
-            return (0, 0)
+   
         
-        if root.left == 0 and root.right == 0:
-            return (0, 0)
-        
-        ld, lh = self.solve(root.left)
-        rd, rh = self.solve(root.right)
-
-        md = max(ld, rd, 2 + lh + rh)
-        mh = 1 + max(lh, rh)
-
-        return (md, mh)
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         # return both d and h
-        return self.solve(root)[0]-2
+       
+        self.diameter = 0
+
+        def solve(node):
+            if node == None:
+                return 0 
+
+            lh = solve(node.left)
+            rh = solve(node.right)
+
+            self.diameter = max(self.diameter, lh + rh)
+
+            return max(lh, rh) + 1
+
+        solve(root)
+        return self.diameter
