c3aa57aaad51dce2df28508cb85fc95673cc68d1
Shardul Parab <shardulSVparab@gmail.com>
2024-04-01 01:38:41 -0400

    Runtime: 41 ms (15.26%), Memory: 16.6 MB (27.38%) - Gitcode

--- 58-length-of-last-word/58. Length of Last Word.py
+++ 58-length-of-last-word/58. Length of Last Word.py
@@ -1,6 +1,12 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        s = s.rstrip()
-        # print(s)
-        return len(s.split(" ")[-1])
-        
+        ans, p = 0, len(s) - 1
+
+        while p >= 0:
+            if s[p] != ' ':
+                ans += 1
+            elif ans>0:
+                return ans
+            p -= 1
+
+        return ans
