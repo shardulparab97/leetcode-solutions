3a3f3859cb7e8b79f5df142e52eb46e66686b2c1
Shardul Parab <shardulSVparab@gmail.com>
2024-04-09 16:19:07 -0400

    Runtime: 417 ms (65.38%), Memory: 31.3 MB (42.00%) - Gitcode

--- 2366-minimum-replacements-to-sort-the-array/2366. Minimum Replacements to Sort the Array.py
+++ 2366-minimum-replacements-to-sort-the-array/2366. Minimum Replacements to Sort the Array.py
@@ -3,7 +3,20 @@ class Solution:
         n = len(nums)
         maxMinVal = nums[n-1]
         ans = 0
-
+        
+        '''
+        For case:
+        maxMinVal = 4
+        nums[i] = 13
+        parts = math.ceil(nums[i] / maxMinVal) = 4
+        ans += 4-1 i.e. 3 different ooperations of splitting
+        
+        now we can split it as 4+4+4+1 or 4+3+3+3 
+        but we want to maintain the maxMinVal as high as possible
+        that's why we pick 4+3+3+3 
+        then we have our new maxMinVal = num[i]//parts = 3
+        '''
+        
         for i in range(n-2, -1, -1):
             parts = math.ceil(nums[i]/maxMinVal)
             ans += (parts - 1)
