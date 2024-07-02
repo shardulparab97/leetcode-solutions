b6368c8e6841eae3537e52665831f6fab118ae35
Shardul Parab <shardulSVparab@gmail.com>
2024-07-01 22:28:20 -0400

    Runtime: 47 ms (56.89%), Memory: 16.7 MB (67.22%) - Gitcode

---
+++ 1550-three-consecutive-odds/1550. Three Consecutive Odds.py
@@ -0,0 +1,8 @@
+class Solution:
+    def threeConsecutiveOdds(self, arr: List[int]) -> bool:
+        cnt = 0 
+        for num in arr:
+            cnt = cnt+1 if num%2==1 else 0
+            if cnt == 3:
+                return True
+        return False
