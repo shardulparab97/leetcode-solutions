5912750c8163b8ff4b779057b739e0f3124b18fa
Shardul Parab <shardulSVparab@gmail.com>
2024-07-06 12:55:20 -0400

    Runtime: 193 ms (5.01%), Memory: 26.7 MB (14.25%) - Gitcode

--- 540-single-element-in-a-sorted-array/540. Single Element in a Sorted Array.py
+++ 540-single-element-in-a-sorted-array/540. Single Element in a Sorted Array.py
@@ -1,26 +1,24 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
         n = len(nums)
-        if n==1:
-            return nums[0]
         lo, hi = 0, n-1
         ans = -1
 
         while lo <= hi:
-            mid = lo + (hi - lo) // 2
-            halves_even = (hi - mid) % 2  == 0
+            mid = lo + (hi - lo)//2
+            even_halves = (hi - mid) % 2 == 0
             if mid+1 < n and nums[mid] == nums[mid+1]:
-                if halves_even:
+                if even_halves:
                     lo = mid + 2
                 else:
                     hi = mid - 1
-            elif mid -1 >= 0 and nums[mid] == nums[mid-1]:
-                if halves_even:
+            elif mid - 1 >= 0 and nums[mid] == nums[mid-1]:
+                if even_halves:
                     hi = mid - 2
                 else:
                     lo = mid + 1
             else:
                 ans = nums[mid]
                 break
+        
         return ans
-            
