9984f66045b568065e5ed1bee62fb987f4b2df25
Shardul Parab <shardulSVparab@gmail.com>
2024-07-07 17:35:31 -0400

    Runtime: 458 ms (28.04%), Memory: 18 MB (72.41%) - Gitcode

--- 127-word-ladder/127. Word Ladder.py
+++ 127-word-ladder/127. Word Ladder.py
@@ -1,30 +1,26 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        
-        
-
         q = collections.deque()
-        q.append([beginWord, 1])
-        vis = set()
-        
+
         wset = set()
         for w in wordList:
             wset.add(w)
-        while q:
+
+        q.append([beginWord, 1])
+        vis = set()
+        vis.add(beginWord)
+        while q: 
             w, cnt = q.popleft()
 
             if w == endWord:
                 return cnt
 
-            # iterate over all the possible options
             for i in range(len(w)):
-                for j in range(0, 26):
-                    if chr(97+j) != w[i]:
-                        wc = w[:i] + chr(97+j) + w[i+1:]
-                        # print(wc)
-                        if wc in wset and wc not in vis:
-                            q.append([wc, cnt+1])
-                            vis.add(wc)
+                for c in range(0, 26):
+                    if chr(97+c) != w[i]:
+                        new_word = w[:i] + chr(97 + c) + w[i+1:]
+                        if new_word in wset and new_word not in vis:
+                            q.append([new_word, cnt+1])
+                            vis.add(new_word)
 
         return 0
-        
