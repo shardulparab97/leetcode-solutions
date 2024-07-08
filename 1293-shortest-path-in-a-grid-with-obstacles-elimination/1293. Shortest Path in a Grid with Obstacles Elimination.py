0eed4d096992128bb39998b12b1a55b612a467d6
Shardul Parab <shardulSVparab@gmail.com>
2024-07-08 08:35:51 -0400

    Runtime: 437 ms (39.82%), Memory: 24.1 MB (41.80%) - Gitcode

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293. Shortest Path in a Grid with Obstacles Elimination.py
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293. Shortest Path in a Grid with Obstacles Elimination.py
@@ -1,32 +1,28 @@
 class Solution:
     def shortestPath(self, grid: List[List[int]], k: int) -> int:
-        m, n = len(grid), len(grid[0])
+        r, c = len(grid), len(grid[0])
         q = collections.deque()
 
-        q.append((0, 0, 0, 0)) # x, y, k, steps
+        q.append((0, 0, k, 0))
         dirs = [[0, 1], [0, -1], [1, 0], [-1, 0]]
         vis = set()
-
+        
         while q:
-            x, y, k_, steps = q.popleft()
-            
-            if (x, y) == (m-1, n-1):
-                return steps
-            
+            x, y, k, dist = q.popleft()
+
+            if (x, y) == (r-1, c-1):
+                return dist
+
             for dir in dirs:
                 nx = x + dir[0]
                 ny = y + dir[1]
-                
 
-                if 0<=nx<m and 0<=ny<n:
-                    k_used = k_
+                if 0<=nx<r and 0<=ny<c:
+                    new_k = k
                     if grid[nx][ny] == 1:
-                        k_used += 1
-                    if k_used<= k and (nx, ny, k_used) not in vis:
-                        q.append((nx, ny, k_used, steps+1))
-                        vis.add((nx, ny, k_used))
-                        
+                        new_k -= 1
+                    if new_k >= 0 and (nx, ny, new_k) not in vis:
+                        q.append((nx, ny, new_k, dist+1))
+                        vis.add((nx, ny, new_k))
 
-        return -1
-            
-
+        return -1 
