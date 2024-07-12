3be94d590f4dc97cf76d85a72ecc4964619ca939
Shardul Parab <shardulSVparab@gmail.com>
2024-07-12 10:19:14 -0400

    Runtime: 838 ms (59.12%), Memory: 65.4 MB (20.34%) - Gitcode

--- 1353-maximum-number-of-events-that-can-be-attended/1353. Maximum Number of Events That Can Be Attended.py
+++ 1353-maximum-number-of-events-that-can-be-attended/1353. Maximum Number of Events That Can Be Attended.py
@@ -1,36 +1,36 @@
 class Solution:
     def maxEvents(self, events: List[List[int]]) -> int:
-        events.sort(key= lambda x: (x[0], x[1]))
-        pq = []
-        ans = 0
-        i =0
+        events.sort(key = lambda x: (x[0], x[1]))
         n = len(events)
+        idx = 0
+        ans = 0
 
+        pq = []
         curr_day = events[0][0]
 
-        while i<n:
-            while i<n and events[i][0] == curr_day:
-                heapq.heappush(pq, events[i][1])
-                i += 1
+        while idx<n:
+            # push all items in heap with st == curr_day
+            while idx <n and events[idx][0] == curr_day: 
+                heapq.heappush(pq, events[idx][1])
+                idx += 1
             
-            # attend the earliest event 
+            # pop earliest ending event and increment curr_day
             heapq.heappop(pq)
             ans += 1
             curr_day += 1
 
-
-
-            # events have ended and can't do anything
-            while pq and pq[0]<curr_day:
+            # pop out all events which have got over before curr_day
+            while pq and pq[0] < curr_day:
                 heapq.heappop(pq)
-            
-            if i<n and not pq:
-                curr_day = events[i][0]
 
-            
+            # if heap is empty, make curr_day equal to the next starting eveny
+            if idx<n and not pq:
+                curr_day = events[idx][0]
+
+        # for remaining items
         while pq:
             if heapq.heappop(pq) >= curr_day:
-                curr_day += 1
                 ans += 1
-        
+                curr_day += 1
+            
         return ans
