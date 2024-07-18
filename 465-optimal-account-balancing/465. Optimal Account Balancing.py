a993a2e5178bf3e1fe9171337fd10101cd45e965
Shardul Parab <shardulSVparab@gmail.com>
2024-07-18 08:40:17 -0400

    Runtime: 43 ms (97.36%), Memory: 16.7 MB (15.48%) - Gitcode

--- 465-optimal-account-balancing/465. Optimal Account Balancing.py
+++ 465-optimal-account-balancing/465. Optimal Account Balancing.py
@@ -1,30 +1,32 @@
 class Solution:
     def minTransfers(self, transactions: List[List[int]]) -> int:
         # https://leetcode.com/problems/optimal-account-balancing/solutions/219187/short-o-n-2-n-dp-solution-with-detailed-explanation-and-complexity-analysis/ 
-        balances = collections.defaultdict(int)
 
-        for t in transactions:
-            balances[t[0]] -= t[2]
-            balances[t[1]] += t[2]
         
-        balances = [v for v in balances.values() if v!=0]
 
-        n = len(balances)
-        dp = [0] * (2**n) # dp[-1] will be when all are set/ all are available
-        sums = [0] * (2**n)
+        balances = collections.defaultdict(int)
+        
+        for giver, receiver, amount in transactions:
+            balances[giver] -= amount
+            balances[receiver] += amount
 
-        for mask in range(2**n): # looping over all the masks
+        amounts = [amount for amount in balances.values() if amount!=0]
+        n = len(amounts)
+
+        dp = [0] * (2**n)
+        sum = [0] * (2**n)
+
+        for mask in range(2**n):
             set_bit = 1
-            for b in range(n): # setting 1 bit at a time
-                if set_bit & mask == 0: # means it was unset previously
+            for i in range(n):
+                if mask & set_bit == 0: # item at set bit not used yet
                     nxt = mask | set_bit
-                    sums[nxt] = sums[mask] + balances[b]
-                    if sums[nxt] == 0: # means we have found a useful subset
-                        dp[nxt] = max(dp[nxt], dp[mask] + 1) 
+                    sum[nxt] = sum[mask] + amounts[i]
+                    if sum[nxt] == 0:
+                        dp[nxt] = max(dp[nxt], dp[mask] + 1)
                     else:
                         dp[nxt] = max(dp[nxt], dp[mask])
-                set_bit = set_bit<<1
+
+                set_bit <<= 1
+
         return n - dp[-1]
-
-
-        
