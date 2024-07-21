6fa6ff746f8bdd7e748cb20ce9b540a58fbb009c
Shardul Parab <shardulSVparab@gmail.com>
2024-07-21 09:43:18 -0400

    Runtime: 43 ms (59.04%), Memory: 16.6 MB (41.77%) - Gitcode

---
+++ 1029-two-city-scheduling/1029. Two City Scheduling.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        costs = sorted(costs, key = lambda x: x[0]-x[1])
+
+        ans = 0
+        n = len(costs)//2
+
+        for i in range(n):
+            ans += costs[i][0] + costs[i+n][1]
+
+        return ans
+
+    
