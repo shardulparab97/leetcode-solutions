dc6bab1bdce88f0881623787bd2913d8e2cf6978
Shardul Parab <shardulSVparab@gmail.com>
2024-07-21 14:46:51 -0400

    Runtime: 1274 ms (67.89%), Memory: 48.1 MB (36.70%) - Gitcode

--- 2050-parallel-courses-iii/2050. Parallel Courses III.py
+++ 2050-parallel-courses-iii/2050. Parallel Courses III.py
@@ -1,29 +1,32 @@
 class Solution:
     def minimumTime(self, n: int, relations: List[List[int]], time: List[int]) -> int:
-        g = [[] for _ in range(n)]
+        # get max time to visit the node and its children
+        # if len(relations) == 0:
+        #     return time[0]
+        g = {i: [] for i in range(1, n+1)}
+        for u, v in relations:
+            g[u].append(v)
 
-        for r in relations:
-            g[r[0]-1].append(r[1]-1)
-
-        # will store distance
         vis = {}
+
         def dfs(node):
             if node in vis:
                 return vis[node]
-            
-            max_len = float("-inf")
-
+        
+            max_time = time[node-1]
             for nei in g[node]:
-                len = dfs(nei)
-                max_len = max(max_len, len + time[node])
-            vis[node] = max_len if max_len != float("-inf") else time[node]
+                nei_time = dfs(nei)
+                max_time = max(max_time, nei_time + time[node-1])
+
+            vis[node] = max_time
             return vis[node]
-                
 
-        ans = float("-inf")
-        for i in range(n):
+
+        ans = -1
+        for i in range(1, n+1):
             if i not in vis:
-                curr_len = dfs(i)
-                ans = max(ans, curr_len)
+                res = dfs(i)
+                ans = max(ans, res)
 
-        return int(ans)
+        return ans
+
