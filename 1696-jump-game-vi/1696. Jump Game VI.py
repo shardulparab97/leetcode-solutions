8dc137301d2295c0bc10bbe098bf824255128f1e
Shardul Parab <shardulSVparab@gmail.com>
2024-07-21 22:44:03 -0400

    Runtime: 682 ms (46.95%), Memory: 30.6 MB (28.91%) - Gitcode

--- 1696-jump-game-vi/1696. Jump Game VI.py
+++ 1696-jump-game-vi/1696. Jump Game VI.py
@@ -1,17 +1,18 @@
 class Solution:
     def maxResult(self, nums: List[int], k: int) -> int:
         n = len(nums)
-        score = [0]*n
+        score = [0] * n
         score[0] = nums[0]
-        dq = deque()
-        dq.append(0)
+        q = collections.deque()
+        q.append(0)
+
         for i in range(1, n):
-            # pop the old index
-            while dq and dq[0] < i-k:
-                dq.popleft()
-            score[i] = score[dq[0]] + nums[i]
-            # pop the smaller value
-            while dq and score[i] >= score[dq[-1]]:
-                dq.pop()
-            dq.append(i)
+            if q and q[0] < i-k:
+                q.popleft()
+            score[i] = nums[i] + score[q[0]]
+
+            while q and score[i] >= score[q[-1]]:
+                q.pop()
+            q.append(i)
+
         return score[-1]
