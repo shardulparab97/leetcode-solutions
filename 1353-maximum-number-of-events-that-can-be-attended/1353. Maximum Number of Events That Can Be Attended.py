0440824145d0bbcc05f5b8499529567f37404769
Shardul Parab <shardulSVparab@gmail.com>
2025-07-07 12:58:15 -0400

    Runtime: 155 ms (48.35%), Memory: 53.2 MB (55.74%) - Gitcode

--- 1353-maximum-number-of-events-that-can-be-attended/1353. Maximum Number of Events That Can Be Attended.py
+++ 1353-maximum-number-of-events-that-can-be-attended/1353. Maximum Number of Events That Can Be Attended.py
@@ -1,36 +1,42 @@
 class Solution:
     def maxEvents(self, events: List[List[int]]) -> int:
-        events.sort(key = lambda x: (x[0], x[1]))
         n = len(events)
-        idx = 0
-        ans = 0
+        events.sort(key = lambda x: (x[0], x[1]))
 
         pq = []
+        ans = 0
         curr_day = events[0][0]
+        idx = 0
 
-        while idx<n:
-            # push all items in heap with st == curr_day
-            while idx <n and events[idx][0] == curr_day: 
+        while idx<n: 
+            
+            # push all events starting before curr_day
+            while idx<n and events[idx][0] == curr_day:
                 heapq.heappush(pq, events[idx][1])
                 idx += 1
-            
-            # pop earliest ending event and increment curr_day
+
+            # complete 1 event
             heapq.heappop(pq)
             ans += 1
             curr_day += 1
 
-            # pop out all events which have got over before curr_day
+            # remove all events ending before curr day
             while pq and pq[0] < curr_day:
                 heapq.heappop(pq)
 
-            # if heap is empty, make curr_day equal to the next starting eveny
-            if idx<n and not pq:
+            # if pq is empty, move curr_day to next starting item 
+            if idx < n and not pq:
                 curr_day = events[idx][0]
 
-        # for remaining items
+        # if the heap is still not empty
         while pq:
             if heapq.heappop(pq) >= curr_day:
                 ans += 1
                 curr_day += 1
-            
+
         return ans
+
+            
+            
+
+            
